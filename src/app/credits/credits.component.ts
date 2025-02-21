b30c889418dd6b8383be0f741891524cbf0c2692
Marta Kalwaj <martaprosniak@gmail.com>
2025-02-21 20:19:51 +0100

    Credits page

--- src/app/credits/credits.component.ts
+++ src/app/credits/credits.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {GameHeaderComponent} from '../game/game-header/game-header.component';
 
 @Component({
   selector: 'app-credits',
-  imports: [],
+  imports: [
+    GameHeaderComponent
+  ],
   templateUrl: './credits.component.html',
   styleUrl: './credits.component.css'
 })
