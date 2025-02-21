459d05a4c874b91f03d210c805c0c67f31c0860d
Marta Kalwaj <martaprosniak@gmail.com>
2025-02-21 19:49:06 +0100

    Change intro top

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {animate, state, style, transition, trigger} from '@angular/animations';
 import {GameHeaderComponent} from '../game/game-header/game-header.component';
 
 @Component({
@@ -11,20 +10,52 @@ import {GameHeaderComponent} from '../game/game-header/game-header.component';
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  animations: [
-    trigger('flyIn', [
-      state('hidden', style({transform: 'translateY(100vh)'})),
-      state('visible', style({transform: 'translateY(0)'})),
-      transition('hidden => visible', animate('2s ease-in-out')),
-    ]),
-  ]
 })
-export class HomeComponent implements AfterViewInit {
-  animationState = 'hidden';
+export class HomeComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('intro', {static: false}) introEl: ElementRef | undefined;
+  startPosition: number = 0;
+  pixelsPerFrame = 2;
+  animationId: number | null = null;
+  maxTop = 0;
+  maxPixelsPerFrame = 16;
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.animationState = 'visible';
-    }, 100)
+    this.setStartPosition();
+    this.animationId = window.requestAnimationFrame(() => this.animate());
   }
+
+  ngOnDestroy() {
+    this.cancelAnimation();
+  }
+
+  speedUpAnimation() {
+    if (this.pixelsPerFrame === this.maxPixelsPerFrame) {
+      return;
+    }
+    this.pixelsPerFrame = this.maxPixelsPerFrame;
+  }
+
+  setStartPosition() {
+    this.startPosition = this.introEl?.nativeElement.offsetTop;
+  }
+
+  cancelAnimation() {
+    if (!!this.animationId) {
+      cancelAnimationFrame(this.animationId);
+    }
+  }
+
+  animate = () => {
+    if (!this.startPosition) {
+      this.animationId = window.requestAnimationFrame(() => this.animate());
+    } else if (this.startPosition >= this.maxTop) {
+      this.startPosition = this.startPosition - this.pixelsPerFrame;
+      if (this.introEl) {
+        this.introEl.nativeElement.style.top = `${this.startPosition}px`;
+      }
+      this.animationId = window.requestAnimationFrame(() => this.animate());
+    } else {
+      this.cancelAnimation();
+    }
+  };
 }
