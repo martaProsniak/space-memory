c9ca18e67514fe1ef6c8dc713d906a8f4a3fc8a3
Marta Kalwaj <martaprosniak@gmail.com>
2025-02-26 20:04:35 +0100

    Fade in animation on home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,13 @@ import {GameHeaderComponent} from '../game/game-header/game-header.component';
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
+  texts = [
+    'A long time ago in a galaxy far far away...',
+    'A young, brave adventurer started his cosmic journey.',
+    'Suddenly... He was attacked by the aliens!',
+    'Now he must rush through their attack line and escape...',
+    'He must shoot two the same targets at once to break one line.',
+    'Would you help him?'
+  ]
 
 }
